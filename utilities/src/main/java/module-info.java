b3882734285f23a0d338d2ad2d235fb88e8c0b94
shalom <shalom938@gmail.com>
2022-03-03 00:45:11 +0200

    reproduced

--- utilities/src/main/java/module-info.java
+++ utilities/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module sample.utilities {
     requires transitive sample.list;
     requires transitive org.apache.logging.log4j;
     requires org.apache.logging.log4j.core;
-//    requires org.apache.logging.log4j.slf4j;
+    requires org.apache.logging.log4j.slf4j;
     requires java.scripting; //required by log4j
     exports org.test.utilities;
 }
